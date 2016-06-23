cd098d2d1fd4a5561f179544489ed6fb4766f1eb
jeffcampbell <jscampbell@etsy.com>
2016-06-23 00:24:44 -0400

    updog

--- app.rb
+++ app.rb
@@ -51,7 +51,7 @@ def generate_text
 end
 
 def generate_attachment
-  replacements = [ ["{", ""],["}", ""],["W", ":white_circle:"], ["U", ":large_blue_circle:"],["B", ":black_circle:"],["R", ":red_circle:"],["G", ":tennis:"] ]
+  replacements = [ ["{W}", ":white_circle:"], ["{U}", ":large_blue_circle:"],["{B}", ":black_circle:"],["{R}", ":red_circle:"],["{G}", ":tennis:"],["{", ""],["}", ""] ]
 
   @cardtext = generate_request[0]["text"]
   @imageurl = generate_request[0]["editions"][0]["image_url"]
